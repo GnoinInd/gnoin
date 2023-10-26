2970b2ab1bf66429ec05b09881f69eabaecae31e
GnoinInd <services@gnoin.com>
2023-10-26 16:05:27 +0530

    changes

--- contact.php
+++ contact.php
@@ -1,3 +1,50 @@
+
+<?php
+
+
+// Create connection
+$conn = mysqli_connect("localhost","root","","gnoin");
+
+// Check the connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = "";
+$email = "";
+$subject = "";
+$message = "";
+
+
+// Check if the form has been submitted
+if(isset($_POST["submit"]))
+{
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // $query = "INSERT INTO `connect`(`id`, `name`, `email`, `discussion`) VALUES ('','$name','$email','$reason')";
+$query = "INSERT INTO contact(id, name, email, subject, message) VALUES ('','$name','$email','$subject','$message')";
+mysqli_query($conn,$query);
+
+// $reload = true;
+}
+
+
+
+
+
+// Close the database connection
+$conn->close();
+?>
+
+
+
+
+
+
   <?php include "header.php"; ?>
 
 
@@ -101,35 +148,36 @@
                 <!--Grid column-->
             </div>
         </section>
-
+        
       
         <!-- location part end -->
 
         <!-- form part start -->
-        <form id="my_form" action="<?php echo $baseUrl ?>contactus" method="POST">
-        <section class="container mt-5">
-            <!--Contact heading-->
-            <div class="row">
+        <!-- <form id="my_form" action="<?php echo $baseUrl ?>contactus" method="POST"> -->
+        <form id="my_form" action="" method="POST">
+           <section class="container mt-5">
+             <!--Contact heading-->
+              <div class="row">
                 <!--Grid column-->
-                <div class="col-sm-12 mb-4 col-md-5">
+                  <div class="col-sm-12 mb-4 col-md-5">
                     <!--Form with header-->
-                    <div class="card border-primary rounded-0">
-                        <div class="card-header p-0">
-                            <div class="bg-primary text-white text-center py-2">
+                      <div class="card border-primary rounded-0">
+                         <div class="card-header p-0">
+                             <div class="bg-primary text-white text-center py-2">
                                 <h3><i class="fa fa-envelope"></i> Write to us:</h3>
                                 <p class="m-0">We’ll write rarely, but only the best content.</p>
                             </div>
-                        </div>
-                        <div class="card-body p-3">
+                         </div>
+                         <div class="card-body p-3">
 
-                            <div class="form-group">
-                                <label> Your name </label>
-                                <div class="input-group">
-                                    <input value="" type="text" name="name" class="form-control"
+                             <div class="form-group">
+                                 <label> Your name </label>
+                                 <div class="input-group">
+                                     <input value="" type="text" name="name" class="form-control"
                                          placeholder="Your name">
-                                </div>
-                            </div>
-                            <div class="form-group">
+                                 </div>
+                             </div>
+                             <div class="form-group">
                                 <label>Your email</label>
                                 <div class="input-group mb-2 mb-sm-0">
                                     <input type="email" value="" name="email" class="form-control"
@@ -156,43 +204,68 @@
 
                         </div>
 
-                    </div>
-                </div>
+                     </div>
+                 </div>
                 <!--Grid column-->
 
                 <!--Grid column-->
-                <div class="col-sm-12 col-md-7">
+                 <div class="col-sm-12 col-md-7">
                     <!--Google map-->
-                    <div class="mb-4">
+                     <div class="mb-4">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3682.86680431503!2d88.46258566418778!3d22.621448536878486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sAstra%20Towers%2C%20ASO%20718%20South%20Block%2C%20Action%20Area%202C%2C%20New%20Town%2C%20Rajarhat%2C%20Kolkata%2C%20West%20Bengal%20-%20700161!5e0!3m2!1sen!2sin!4v1672747048977!5m2!1sen!2sin"
                             width="100%" height="450"  allowfullscreen="" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                    <!--Buttons-->
-                    <div class="row text-center">
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                     </div>
+                     <!--Buttons-->
+                     <div class="row text-center">
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-map-marker"></i></a>
-                            <p> Gnoin Pvt. Ltd.</p>
-                        </div>
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                             <p> Gnoin Pvt. Ltd.</p>
+                         </div>
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-phone"></i></a>
-                            <p>+91-33-48040710</p>
-                        </div>
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                             <p>+91-33-48040710</p>
+                         </div>
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-envelope"></i></a>
-                            <p> Info@Gnoin.Com</p>
-                        </div>
-                    </div>
-                </div>
+                             <p> Info@Gnoin.Com</p>
+                          </div>
+                      </div>
+                  </div>
                 <!--Grid column-->
-            </div>
-        </section>
+              </div>
+           </section>
          </form>
         <!-- form part end -->
+
+
+        <script>
+    // Add an event listener to the form's submit event
+    document.getElementById("my_form").addEventListener("submit", function (e) {
+        // Check if the required fields are empty
+        if (!document.querySelector('input[name="name"]').value || !document.querySelector('input[name="email"]').value || !document.querySelector('input[name="subject"]').value || !document.querySelector('input[name="message"]').value) {
+            alert("Please fill in all required fields.");
+            e.preventDefault(); // Prevent form submission
+        } else {
+            // Display a success message
+            alert("Data submitted successfully.");
+
+             // Reload the page
+             location.reload();
+
+            // Prevent the page from reloading
+            e.preventDefault();
+        }
+    });
+</script>
+
+
+
+
 
 
         <section class="cta-one">
