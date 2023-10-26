09833a85826f687b0bca0a9aa2cc54a954e06e75
golamdeveloper <144772085+golamdeveloper@users.noreply.github.com>
2023-10-26 17:31:32 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+
+// Create connection
+$conn = mysqli_connect("localhost","root","","gnoin");
+
+// Check the connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
