f64dbc608a7586426ac34e029fee6c7f4950d77e
unknown <services@gnoin.com>
2023-12-26 20:45:54 +0530

    testing changes added

--- contact.php
+++ contact.php
@@ -1,42 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/logo.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/logo.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/logo.png">
-    <link rel="manifest" href="images/favicon/site.webmanifest">
-    <title>Gnoin Pvt Ltd </title>
-    <link href="../css?family=Open+Sans:400,400i,600,600i,700,700i%7CWork+Sans:400,500,600,700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/hover-min.css">
-    <link rel="stylesheet" href="css/vegas.min.css">
-    <link rel="stylesheet" href="plugins/inchtech-icons/style.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+<?php
 
-    <!-- <link href="https://fonts.cdnfonts.com/css/verdana" rel="stylesheet"> -->
-</head>
 
-<body class="home-one">
-    <div class="preloader"><span></span></div><!-- /.preloader -->
-    <div class="page-wrapper">
-        <!-- header part start -->
-        <?php
+//server connection
+//$conn = mysqli_connect("localhost","gnoin","Gnoin2023","gnoin");
 
-include("header.php");
+
+// Create connection
+$conn = mysqli_connect("localhost","root","","gnoin");
+
+// Check the connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = "";
+$email = "";
+$subject = "";
+$message = "";
+
+
+// Check if the form has been submitted
+if(isset($_POST["submit"]))
+{
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // $query = "INSERT INTO `connect`(`id`, `name`, `email`, `discussion`) VALUES ('','$name','$email','$reason')";
+$query = "INSERT INTO contact(id, name, email, subject, message) VALUES ('','$name','$email','$subject','$message')";
+mysqli_query($conn,$query);
+
+// $reload = true;
+}
+
+
+
+
+
+// Close the database connection
+$conn->close();
 ?>
+
+
+
+
+
+
+  <?php include "header.php"; ?>
+
+
         <!-- header end -->
 
 
@@ -49,8 +64,8 @@ include("header.php");
                     <p class="text-light">Believe in vibrant minds, smart ideas and coordinated
                         change.</p>
                     <ul class="thm-breadcrumb list-unstyled">
-                        <li><a href="index.php">Home</a></li>
-                        <li class="active"><a href="contact.php">Contact</a></li>
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active"><a href="contact.html">Contact</a></li>
                     </ul>
                 </div>
             </div>
@@ -89,11 +104,17 @@ include("header.php");
                     <!--Google map-->
                     <div class="card  rounded-0 bg-primary text-white text-center py-2">
                         <div class=" p-3">
+                              <div class="d-flex flex-column justify-content-center align-items-start ">
+                                
+                                <h5 class="ml-2">Gnoin Private Limited</h5>
+                                
+                                <p class="text-light ">Best Website Design, Software Development and Digital Marketing Company Kolkata</p>
+                            </div>
                             <div class="d-flex flex-column justify-content-center align-items-start ">
                                 <h5 class="ml-2">Address
 
                                 </h5>
-                                <p class="text-light ">Astra Towers, ASO 718 South Block, Action Area 2C,<br> New
+                                <p class="text-light ">Astra Towers, ASO 432, 4th Floor, South Block, Action Area 2C,<br> New
                                     Town, Rajarhat, Kolkata, West Bengal - 700161</p>
                             </div>
                             <div class="d-flex flex-column justify-content-center align-items-start ">
@@ -136,59 +157,53 @@ include("header.php");
                 <!--Grid column-->
             </div>
         </section>
-
+        
       
         <!-- location part end -->
 
         <!-- form part start -->
-        <section class="container mt-5">
-            <!--Contact heading-->
-            <div class="row">
+        <!-- <form id="my_form" action="<?php echo $baseUrl ?>contactus" method="POST"> -->
+        <form id="my_form" action="" method="POST">
+           <section class="container mt-5">
+             <!--Contact heading-->
+              <div class="row">
                 <!--Grid column-->
-                <div class="col-sm-12 mb-4 col-md-5">
+                  <div class="col-sm-12 mb-4 col-md-5">
                     <!--Form with header-->
-                    <div class="card border-primary rounded-0">
-                        <div class="card-header p-0">
-                            <div class="bg-primary text-white text-center py-2">
+                      <div class="card border-primary rounded-0">
+                         <div class="card-header p-0">
+                             <div class="bg-primary text-white text-center py-2">
                                 <h3><i class="fa fa-envelope"></i> Write to us:</h3>
-                                <p class="m-0 text-white">We’ll write rarely, but only the best content.</p>
+                                <p class="m-0">We’ll write rarely, but only the best content.</p>
                             </div>
-                        </div>
-                        <div class="card-body p-3">
+                         </div>
+                         <div class="card-body p-3">
 
-                            <div class="form-group">
-                                <label> Your name </label>
-                                <div class="input-group">
-                                    <input value="" type="text" name="data[name]" class="form-control"
-                                        id="inlineFormInputGroupUsername" placeholder="Your name">
-                                </div>
-                            </div>
-                            <div class="form-group">
+                             <div class="form-group">
+                                 <label> Your name </label>
+                                 <div class="input-group">
+                                     <input value="" type="text" name="name" class="form-control"
+                                         placeholder="Your name">
+                                 </div>
+                             </div>
+                             <div class="form-group">
                                 <label>Your email</label>
                                 <div class="input-group mb-2 mb-sm-0">
-                                    <input type="email" value="" name="data[email]" class="form-control"
-                                        id="inlineFormInputGroupUsername" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Your phone</label>
-                                <div class="input-group mb-2 mb-sm-0">
-                                    <input type="number" value="" name="data[phone]" class="form-control"
-                                        id="phone" placeholder="Phone Number">
+                                    <input type="email" value="" name="email" class="form-control"
+                                          placeholder="Email">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label>Subject</label>
                                 <div class="input-group mb-2 mb-sm-0">
-                                    <input type="text" name="data[subject]" class="form-control"
-                                        id="inlineFormInputGroupUsername" placeholder="Subject">
+                                    <input type="text" name="subject" class="form-control"
+                                          placeholder="Subject">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label>Message</label>
                                 <div class="input-group mb-2 mb-sm-0">
-                                    <textarea name="mesg" id="mesg"class="form-control" cols="10" rows="2"></textarea>
-                                    
+                                    <input type="text" class="form-control" name="message">
                                 </div>
                             </div>
                             <div class="text-center">
@@ -198,40 +213,68 @@ include("header.php");
 
                         </div>
 
-                    </div>
-                </div>
-               
-                <div class="col-sm-12 col-md-7">
-                   
-                    <div class="mb-4">
+                     </div>
+                 </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                 <div class="col-sm-12 col-md-7">
+                    <!--Google map-->
+                     <div class="mb-4">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3682.86680431503!2d88.46258566418778!3d22.621448536878486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sAstra%20Towers%2C%20ASO%20718%20South%20Block%2C%20Action%20Area%202C%2C%20New%20Town%2C%20Rajarhat%2C%20Kolkata%2C%20West%20Bengal%20-%20700161!5e0!3m2!1sen!2sin!4v1672747048977!5m2!1sen!2sin"
                             width="100%" height="450"  allowfullscreen="" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                   
-                    <div class="row text-center">
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                     </div>
+                     <!--Buttons-->
+                     <div class="row text-center">
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-map-marker"></i></a>
-                            <p> Gnoin Pvt. Ltd.</p>
-                        </div>
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                             <p> Gnoin Pvt. Ltd.</p>
+                         </div>
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-phone"></i></a>
-                            <p>+91-33-48040710</p>
-                        </div>
-                        <div class="col-md-4">
-                            <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
+                             <p>+91-33-48040710</p>
+                         </div>
+                         <div class="col-md-4">
+                             <a class="bg-primary px-3 py-2 rounded text-white mb-2 d-inline-block"><i
                                     class="fa fa-envelope"></i></a>
-                            <p> Info@Gnoin.Com</p>
-                        </div>
-                    </div>
-                </div>
-               
-            </div>
-        </section>
-        
+                             <p> Info@Gnoin.Com</p>
+                          </div>
+                      </div>
+                  </div>
+                <!--Grid column-->
+              </div>
+           </section>
+         </form>
+        <!-- form part end -->
+
+
+        <script>
+    // Add an event listener to the form's submit event
+    document.getElementById("my_form").addEventListener("submit", function (e) {
+        // Check if the required fields are empty
+        if (!document.querySelector('input[name="name"]').value || !document.querySelector('input[name="email"]').value || !document.querySelector('input[name="subject"]').value || !document.querySelector('input[name="message"]').value) {
+            alert("Please fill in all required fields.");
+            e.preventDefault(); // Prevent form submission
+        } else {
+            // Display a success message
+            alert("Data submitted successfully.");
+
+             // Reload the page
+             location.reload();
+
+            // Prevent the page from reloading
+            e.preventDefault();
+        }
+    });
+</script>
+
+
+
+
 
 
         <section class="cta-one">
@@ -251,26 +294,5 @@ include("header.php");
 
     </div><!-- /.page-wrapper -->
 
-    <?php
 
-include("footer.php");
-?>
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/TweenMax.min.js"></script>
-    <script src="js/jquery.validate.min.js"></script>
-    <script src="js/bootstrap-select.min.js"></script>
-    <script src="js/bootstrap-datepicker.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/isotope.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/vegas.min.js"></script>
-    <script src="js/theme.js"></script>
-    <script src="./js/index.js"></script>
-</body>
-
-</html>
+      <?php include("footer.php"); ?>
--- server.php
+++ server.php
@@ -2,7 +2,8 @@
 //echo "<pre>";
 //echo $_SERVER['PHP_SELF'];
 //echo $_SERVER['SCRIPT_FILENAME'];
-//echo $_SERVER['DOCUMENT_ROOT'];
-//echo $_SERVER['SERVER_NAME'];
+echo $_SERVER['DOCUMENT_ROOT'];
+echo $_SERVER['SERVER_NAME'];
+echo "Hello World";
 
 ?>
