d70843cee01fc05054b7c2d69e1c03f89f6c2f83
GnoinInd <128312522+GnoinInd@users.noreply.github.com>
2024-01-27 15:37:25 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -56,7 +56,7 @@
                                     <li><a href="./on-demand-developer.php" previewlistener="true">On Demand developers </a></li>
                                     <li><a href="./about.php" previewlistener="true">Blog</a></li>
                                     <li><a href="./contact.php" previewlistener="true">Careers</a></li>
-                                    <li><a id="privacyPolicy" previewlistener="true">Privacy Policy</a></li>
+                                    <li><a class="privacyPolicy" previewlistener="true">Privacy Policy</a></li>
                                     <li><a  class="RefundCancellationPolicy" previewlistener="true">Refund Cancellation Policy</a></li>
                                     </ul>
                                     <!-- /.footer-widget__links -->
@@ -131,7 +131,6 @@
                         </div>
                         <ul class="list-unstyled site-footer__links">
                            <li><a  class="termsOfService">Terms of Service</a></li>
-                        
                             <li><a  class="privacyPolicy">Privacy Policy</a></li>
                          <li><a  class="RefundCancellationPolicy">Refund Cancellation Policy</a></li>
                         </ul>
